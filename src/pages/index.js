53a15f7d685f431d19ce78cfa74bd5d18fd1727a
Gavin Webster <gw@exemail.com.au>
2021-01-27 08:41:19 +1100

    updated files

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,17 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <ol>Things to do:
+    <li>Use Open Sans Font</li>
+    <li>Add Header</li>
+    <li>Add Logo to Header</li>
+    <li>Add Header Menu</li>
+    <li>Add Fullscreen Background Blurred</li>
+    <li>Add Log in & Sign up buttons</li>
+    <li>Add Popup</li>
+    <li>Add Footer + Privacy Policy link + Language Dropdown</li>
+    </ol>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
